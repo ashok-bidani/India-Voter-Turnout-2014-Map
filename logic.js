5ae0bf20008b62a3128e5b20d35cc457ca694730
Ashok Bidani <ashok.bidani.321@gmail.com>
2019-06-29 01:03:10 -0500

    Add tooltip

--- logic.js
+++ logic.js
@@ -21,7 +21,11 @@ var legendLabels = ["50-58%", "58-66%", "66-74%", "74-82%", "Over 82%"]
 var color = d3.scaleThreshold()
                 .domain(colorDomain)
                 .range(["#ffffcc","#a1dab4","#41b6c4","#2c7fb8","#253494"]);
-        
+
+// Add tooltip
+var div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+
 // If loading only parliamentary constituency background map
 /*d3.json("data/india_pc_map_2014_merged.geojson").then(function(data) {
     svg.selectAll("path")
@@ -34,24 +38,26 @@ var color = d3.scaleThreshold()
 // Draw choropleth map in single function. First load voter turnout data
 d3.csv("data/india_pc_voter_turnout_2014.csv").then(function(data) {
             
-    // Load GeoJSON data
+    // Load GeoJSON data of parliamentary constituencies (PC's)
     d3.json("data/india_pc_map_2014_merged.geojson").then(function(json) {
             
         // Bind GeoJSON data to voter turnout data
         for (var i = 0; i < data.length; i++) {
             var dataState = data[i].state;
             var dataPCNo = data[i].pcNo;
+            var dataPCName = data[i].pcName;
             var dataValue = parseFloat(data[i].voterTurnout);
             for (var j = 0; j < json.features.length; j++) {
 				var jsonState = json.features[j].properties.state;
                 var jsonPCNo = json.features[j].properties.pc;
 				if (dataState == jsonState && dataPCNo == jsonPCNo) {
-				    json.features[j].properties.voterTurnout = dataValue;
-				    break;
+                    json.features[j].properties.voterTurnout = dataValue;
+                    json.features[j].properties.pcName = dataPCName;
+                    break;
 				}
             }		
         }
-                
+        
         // Draw the map
         svg.selectAll("path")
             .data(json.features)
@@ -62,7 +68,18 @@ d3.csv("data/india_pc_voter_turnout_2014.csv").then(function(data) {
                 var value = d.properties.voterTurnout;
                 if (value) {return color(value); } else {return "#ccc"; }
             })
-            .style("opacity", 0.8);
+            .style("opacity", 0.7)
+        
+            // Mouse events
+            .on("mouseover", function(d) {
+                d3.select(this).style("opacity", 1);
+                div.text(d.properties.pcName + ": " + d.properties.voterTurnout)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY -30) + "px");
+            })
+            .on("mouseout", function(d) {
+                d3.select(this).style("opacity", 0.7);
+            });
                 
     });
 });
@@ -80,8 +97,8 @@ legend.append("rect")
     .attr("y", function(d, i) {return height - (i*legendSquare) - 2*legendSquare; })
     .attr("width", legendSquare)
     .attr("height", legendSquare)
-    .style("fill", function(d, i) {return color(d); })
-    .style("opacity", 0.8);
+    .style("fill", function(d) {return color(d); })
+    .style("opacity", 0.7);
 
 legend.append("text")
     .attr("x", width - 70)
